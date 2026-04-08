221d78bcdfed65bf3d7a2098e599c1853c55e2e4
FraualphaMar <257962461+FraualphaMar@users.noreply.github.com>
2026-04-08 10:41:26 +0800

    Add event listeners to the operators and digits buttons

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function divide(a, b) {
 
 // calculator operation flow
 
-
 function operate(a, operation, b) {
     if (operation === "divide" && b === 0) {
         return "ERROR, cannot divide by zero";
@@ -29,4 +28,27 @@ function operate(a, operation, b) {
     }
 }
 
+const digitsBtn = document.querySelectorAll(".digits")
+const operatorsBtn = document.querySelectorAll(".operators");
 
+digitsBtn.forEach(button => {
+    button.addEventListener("click", () => {
+
+    })
+});
+
+operatorsBtn.forEach(button => {
+    button.addEventListener("click", () => {
+    })
+})
+
+const equalsBtn = document.querySelector(".equals");
+const clearBtn = document.querySelector(".clear");
+
+clearBtn.addEventListener("click", () => {
+
+})
+
+equalsBtn.addEventListener("click", () => {
+
+})
